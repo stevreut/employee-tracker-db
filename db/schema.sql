c2b1ef247a954970b047fd5c16b3e1063d7f05af
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-27 09:18:00 -0400

    foreign keys added to schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,6 @@ CREATE DATABASE hr_db;
 
 USE hr_db;
 
--- TODO - must still add foreign keys (and internal keys) --
-
 CREATE TABLE department (
     id INT NOT NULL auto_increment PRIMARY KEY,
     name VARCHAR(30) NOT NULL
@@ -14,15 +12,21 @@ CREATE TABLE role (
     id INT NOT NULL auto_increment PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INT NOT NULL auto_increment PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT  -- could be NULL for CEO
+    role_id INT,
+    manager_id INT,  -- could be NULL for CEO
+    FOREIGN KEY (role_id) REFERENCES role(id)
+        ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+        ON DELETE SET NULL
 );
 
 
